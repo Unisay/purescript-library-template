9309067c835ce91de81c825ffded338f77202c31
Yuriy Lazarev <unisay@users.noreply.github.com>
2022-08-27 13:12:20 +0200

    Example spec

--- spago.dhall
+++ spago.dhall
@@ -1,5 +1,5 @@
 { name = "my-project"
-, dependencies = [ "console", "effect", "prelude" ]
+, dependencies = [ "aff", "console", "effect", "prelude", "spec" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 }
--- test/Main.purs
+++ test/Main.purs
@@ -3,9 +3,15 @@ module Test.Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Class.Console (log)
+import Effect.Aff (launchAff_)
+import Test.Spec (describe, it)
+import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Reporter (consoleReporter)
+import Test.Spec.Runner (runSpec)
 
-main :: Effect Unit
-main = do
-  log "🍝"
-  log "You should add some tests."
+main ∷ Effect Unit
+main = launchAff_ $ runSpec [ consoleReporter ] do
+  describe "Example Suite" do
+    describe "Nested Suite" do
+      it "example test" do
+        true `shouldEqual` true
